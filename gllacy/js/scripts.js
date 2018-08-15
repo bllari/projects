3b8cf114f811f5d4ed728f904e26357c92dfb1e8
bllari <gidnest@gmail.com>
2018-08-16 01:39:50 +0300

    1

--- gllacy/js/scripts.js
+++ gllacy/js/scripts.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
     cssEase: "linear",
   });
 });
-
 $(function() {
 	$('#slick-slide-control01').on('click', function() {
     	$('body').css('background-color', "#8996a6");
@@ -30,4 +29,37 @@ $(function() {
     	$('body').css('background-color', "#9d8b84");
       $('body').css('background-image', "url(img/slider3.png)");
     });
+});
+
+    ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [59.938870, 30.325791],
+            zoom: 17,
+            controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark([59.938662, 30.323079], {
+            hintContent: 'Shop Gllacy',
+            balloonContent: 'Ул. Большая Конюшенная 19/8'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/metka.png',
+            // Размеры метки.
+            iconImageSize: [218, 142],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-45, -140]
+        });
+    myMap.geoObjects
+        .add(myPlacemark);
 });
